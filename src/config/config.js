7dc5ebdb2fca453e99f1d5c6573b0689b963f0e5
Bana Codes <banacodes@gmail.com>
2024-12-27 13:44:18 +0300

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -2,8 +2,15 @@ require('dotenv').config();
 
 const config = {
   token: process.env.TELEGRAM_BOT_TOKEN,
-  destinations: process.env.TELEGRAM_DESTINATIONS.split(',').map(dest => dest.trim()),
+  destinations: process.env.TELEGRAM_DESTINATIONS ? 
+    process.env.TELEGRAM_DESTINATIONS.split(',').map(dest => dest.trim()) : 
+    [],
   defaultChannel: '@TopHackersNewsDaily'
 };
+
+// Add some validation
+if (!config.token) {
+  throw new Error('TELEGRAM_BOT_TOKEN is required');
+}
 
 module.exports = { config };
