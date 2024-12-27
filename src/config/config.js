315f7f9bc8bcabe5a0d6ceb1ad336e2853780918
Bana Codes <banacodes@gmail.com>
2024-12-27 13:02:47 +0300

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const config = {
-  botToken: process.env.TELEGRAM_BOT_TOKEN,
+  token: process.env.TELEGRAM_BOT_TOKEN,
   destinations: process.env.TELEGRAM_DESTINATIONS.split(',').map(dest => dest.trim()),
   defaultChannel: '@TopHackersNewsDaily'
 };
