7790cefacb266399e4763d7d2e812c453547f50e
Bana Codes <banacodes@gmail.com>
2025-01-24 20:41:04 +0300

    Update scheduler.js

--- src/utils/scheduler.js
+++ src/utils/scheduler.js
@@ -6,7 +6,7 @@ const EAST_AFRICAN_TIMEZONE = 'Africa/Addis_Ababa';
 const scheduleDaily = (taskFn) => {
   const rule = new schedule.RecurrenceRule();
   rule.hour = 20;
-  rule.minute = 35;
+  rule.minute = 45;
   rule.tz = EAST_AFRICAN_TIMEZONE;
 
   return schedule.scheduleJob(rule, async () => {
