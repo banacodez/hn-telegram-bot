01267039fde7d188b7a327b718770e09f43508f0
Bana Codes <banacodes@gmail.com>
2025-07-30 08:09:35 +0300

    Update cron.js

--- api/cron.js
+++ api/cron.js
@@ -6,10 +6,9 @@ const { sendToDestinations } = require('../src/services/messageService');
 const { logger } = require('../src/utils/logger');
 
 // This is a Vercel Serverless Function
-module.exports = async (req, res) => {
+module.exports = async (request, response) => {
   try {
     logger.info('CRON: Starting daily update task.');
-    // Instantiate a new bot instance for this task
     const bot = new TelegramBot(config.token);
 
     const stories = await fetchTopStories();
@@ -17,11 +16,9 @@ module.exports = async (req, res) => {
     await sendToDestinations(bot, message, config.destinations);
 
     logger.info('CRON: Daily update task completed successfully.');
-    // Let Vercel know the job succeeded
-    res.status(200).send('Daily update sent successfully.');
+    response.status(200).send('Daily update sent successfully.');
   } catch (error) {
     logger.error('CRON: Error in daily update task:', error);
-    // Let Vercel know the job failed
-    res.status(500).json({ error: 'Failed to send daily update.' });
+    response.status(500).json({ error: 'Failed to send daily update.' });
   }
 };
